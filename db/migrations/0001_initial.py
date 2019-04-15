b8a322227078ebe66760e7755831bda0e85e3f4e
Dylan Lade <dylanlade98@gmail.com>
2019-04-15 16:59:40 -0500

    Added Dummy data

--- db/migrations/0001_initial.py
+++ db/migrations/0001_initial.py
@@ -14,7 +14,6 @@ operations = [
 
         CREATE TABLE `account` (
             `userId` bigint(20) UNSIGNED NOT NULL,
-            `controllerId` bigint(20) UNSIGNED NOT NULL,
             `firstName` varchar(50) NOT NULL,
             `lastName` varchar(50) NOT NULL,
             `privilege` set('BASIC','ADVANCED','ADMIN') NOT NULL DEFAULT 'BASIC',
@@ -25,17 +24,16 @@ operations = [
         -- --------------------------------------------------------
 
         --
-        -- Table structure for table `adjustment`
+        -- Table structure for table `controller`
         --
 
-        CREATE TABLE `adjustment` (
+        CREATE TABLE `controller` (
             `controllerId` bigint(20) UNSIGNED NOT NULL,
+            `location` varchar(50) NOT NULL,
+            `privilege` set('BASIC','ADVANCED','ADMIN') NOT NULL DEFAULT 'BASIC',
             `state` tinyint(1) NOT NULL,
             `brightness` int(11) NOT NULL,
-            `red` int(11) NOT NULL,
-            `blue` int(11) NOT NULL,
-            `green` int(11) NOT NULL,
-            `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `color` BINARY(3) NOT NULL DEFAULT 'NULL',
             PRIMARY KEY (`controllerId`)
         );
 
@@ -64,22 +62,6 @@ operations = [
             `status` tinyint(1) NOT NULL,
             `cameraId` bigint(20) UNSIGNED NOT NULL,
             PRIMARY KEY (`status`)
-        );
-
-        -- --------------------------------------------------------
-
-        --
-        -- Table structure for table `light`
-        --
-
-        CREATE TABLE `light` (
-            `controllerId` bigint(20) UNSIGNED NOT NULL,
-            `state` tinyint(1) NOT NULL,
-            `brightness` int(11) NOT NULL,
-            `red` int(11) NOT NULL,
-            `blue` int(11) NOT NULL,
-            `green` int(11) NOT NULL,
-            PRIMARY KEY (`controllerId`)
         );
 
         -- --------------------------------------------------------
@@ -144,8 +126,104 @@ operations = [
         -- Dummy Data
         --
 
+        -- Accounts
+
         INSERT INTO `account` (
-        )
+            `userId`, `firstName`, `lastName`, `privilege`, `created`)
+        VALUES (
+            '123456789', 'Joe', 'Smith', 'ADMIN', '2019-04-10 15:15:31');
+
+        INSERT INTO `account` (
+            `userId`, `firstName`, `lastName`, `privilege`, `created`)
+        VALUES (
+            '987654321', 'Chip', 'Smith', 'BASIC', '2019-04-14 14:37:12');
+
+        INSERT INTO `account` (
+            `userId`, `firstName`, `lastName`, `privilege`, `created`)
+        VALUES (
+            '12344321', 'Chucky', 'Smith', 'BASIC', '2019-04-14 14:23:48');
+
+        INSERT INTO `account` ( 
+            `userId`, `firstName`, `lastName`, `privilege`, `created`)
+        VALUES ( 
+            '43211234', 'Jane', 'Smith', 'ADVANCED', '2019-04-10 15:37:24');
+
+        INSERT INTO `account` (
+            `userId`, `firstName`, `lastName`, `privilege`, `created`)
+        VALUES (
+            '19451945', 'Agitha', 'Smith', 'BASIC', '2019-04-14 13:45:33');
+
+
+        -- Front Door
+
+        INSERT INTO `frontdoor` (
+            `status`, `cameraId`)
+        VALUES (
+            '0', '456456456');
+
+        INSERT INTO `camera` (
+            `cameraId`, `sensorId`, `photoStorage`, `recording`)
+        VALUES (
+            '456456456', '654654654', '/photos/visitors', '0');
+
+        INSERT INTO `photo` (
+            `photoId`, `photoStorage`, `created`)
+        VALUES (
+            '879456123', '/photos/visitors', '2019-04-11 12:32:11');
+
+        -- Television
+
+        INSERT INTO `television` (
+            `deviveId`, `name`)
+        VALUES (
+            '888555222', 'Living Room');
+
+        INSERT INTO `television` (
+            `deviceId`, `name`)
+        VALUES ( 
+            '777444111', 'Kitchen');
+
+        INSERT INTO `televisionshow` (
+            `showName`, `start`, `end`, `ratingRestriction`)
+        VALUES(
+            'The Office', '2019-04-14 13:10:34', '2019-04-14 16:32;11', 1);
+
+        INSERT INTO `televisionshow` (
+            `showName`, `start`, `end`, `ratingRestriction`)
+        VALUES (
+            'SpongeBob', '2019-04-15 08:24:33', '2019-04-15 09:13:22', 0);
+
+        INSERT INTO `televisionshow` (
+            `showName`, `start`, `end`, `ratingRestriction`)
+        VALUES ( 
+            'Game Of Thrones', '2019-04-14 20:12:44', '2019-04-14 22:12:32', 3);
+
+        INSERT INTO `restriction` ( 
+            `restrictionId`, `deviceId`, `userId`, `dayOfWeek`, `start`, `end`, `ratingRestriction`)
+        VALUES (
+            '555555555', '888555222', '987654321', 'Friday', '05:00:00', '09:00:00', 1);
+
+        INSERT INTO `restriction` (
+            `restrictionId`, `deviceId`, `userId`, `dayOfWeek`, `start`, `end`, `ratingRestriction`)
+        VALUES (
+            '444444444', '888555222', '12344321', 'Friday', '04:00:00', '09:00:00', 1);
+
+        -- Lights
+
+        INSERT INTO `controller` (
+            `controllerId`, `location`, `privilege`, `state`, `brightness`)
+        VALUES (
+            '777555333', 'living Room', 'ADVANCED', '1', '100');
+
+        INSERT INTO `controller` (
+            `controllerId`, `location`, `privilege`, `state`, `brightness`, `color`)
+        VALUES (
+            '999555111', 'Master Bedroom', 'ADVACED', '1', '75', UNHEX('ff0000'));
+
+        INSERT INTO `controller` (
+            `controllerId`, `location`, `privilege`, `state`, `brightness`, `color`)
+        VALUES (
+            '777555111', 'Chip's Room', 'BASIC', '0', '100', UNHEX('003aff'));
 
       """,
         reverse_sql="""
